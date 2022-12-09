49a4c3c58063848410302c6c6b20d98596eb36af
badrik0077 <107414974+badrik0077@users.noreply.github.com>
2022-12-09 10:20:39 +0500

    pep 8

--- utils/checker.py
+++ utils/checker.py
@@ -1,7 +1,7 @@
 from typing import Optional, Union, Any, Dict, List
-
 from flask import abort
 from werkzeug.datastructures import MultiDict
+from flask import Flask, abort, request, Response
 
 
 def args_reader(**kwargs: Dict[str, str]) -> tuple:
@@ -13,18 +13,32 @@ def args_reader(**kwargs: Dict[str, str]) -> tuple:
 
     if not (cmd1 := kwargs.get('cmd1')):
         abort(400, 'Main command not found')
+
     if not (value1 := kwargs.get('value1')):
         abort(400, 'Value for main command not found')
 
-    cmd2: Optional[str] = kwargs.get('cmd2')
-    value2: Optional[str] = kwargs.get('value2')
-    return cmd1, value1, cmd2, value2, file_name
+    try:
+        file_name = request.args['file_name']
+        cmd1 = request.args['cmd1']
+        value1 = request.args['value1']
+        cmd2 = request.args['cmd2']
+        value2 = request.args['value2']
+    except KeyError:
+        return Response({'error': 'Wrong arguments'}, 400)
+
+    #cmd2: Optional[str] = kwargs.get('cmd2')
+    #value2: Optional[str] = kwargs.get('value2')
+    #return cmd1, value1, cmd2, value2, file_name
+
 
 def commands_checker(cmd1: str, cmd2: str) -> None:
     available_commands: tuple = ('filter', 'map', 'unique', 'limit', 'sort', 'regex')
+
     if cmd1 not in available_commands:
         abort(400, 'Main command not found')
+
     if not cmd2:
         return
+
     if cmd2 not in available_commands:
         abort(400, 'Optional command not found')
